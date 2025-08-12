27e45c46f3b9c437a9491bfcb62dc3fd15bf9fdf
samuelodoom004 <samuel.odoom004@stu.ucc.edu.gh>
2025-08-12 23:30:54 +0000

    Add files via upload

---
+++ john.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // for (int i = 0; i < 10; i++) {
+  //   if (i == 4) {
+  //     continue;
+  //   }
+  //   cout << i << "\n";
+  // } 
+  // return 0;
+cout << (3 > 2 && 4 > 3);
+  return 0;
+}
